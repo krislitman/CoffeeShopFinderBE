de050b288e30e35ede8161843a919a4539891263
Kris Litman <kris.d.litman@gmail.com>
2021-06-27 19:24:31 -0400

    Can create coffee shop and favorite shop that is linked to a user

--- app/api/views.py
+++ app/api/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import CoffeeShop, FavoriteShop
+from django.contrib.auth.models import User
 from .serializers import CoffeeShopSerializer, FavoriteShopSerializer
 
 
@@ -8,7 +11,44 @@ class CoffeeShopViewSet(viewsets.ModelViewSet):
     queryset = CoffeeShop.objects.all()
     serializer_class = CoffeeShopSerializer
 
+    @action(detail=True, methods=['POST'])
+    def create_favorite(self, request, pk=None):
+        if 'rating' in request.data:
+            current_shop = CoffeeShop.objects.get(id=pk)
+            # TODO Will need to change this to current user, not 1
+            current_user = User.objects.get(id=1)
+            rating = request.data['rating']
+            FavoriteShop.objects.create(
+                user=current_user,
+                coffeeshop=current_shop,
+                rating=rating
+            )
+            response = {
+                'message': 'Favorite Shop Added'
+            }
+            return Response(response, status=status.HTTP_201_CREATED)
+        else:
+            response = {
+                'message': 'You must provide a rating'
+            }
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
 
 class FavoriteShopViewSet(viewsets.ModelViewSet):
     queryset = FavoriteShop.objects.all()
     serializer_class = FavoriteShopSerializer
+
+    @action(detail=True, methods=['POST'])
+    def rate_shop(self, request, pk=None):
+        if 'rating' in request.data:
+            shop = FavoriteShop.objects.get(id=pk)
+            shop.rating = request.data['rating']
+            response = {
+                'message': 'Rating updated'
+            }
+            return Response(response, status=status.HTTP_200_OK)
+        else:
+            response = {
+                'message': 'You must provide a rating'
+            }
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
