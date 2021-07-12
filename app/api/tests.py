6ce076547a93606fbfc1cb21f6174382bbe481df
Kris Litman <kris.d.litman@gmail.com>
2021-07-12 12:37:38 -0400

    Added request tests for Coffeeshop all, and for a single shop

--- app/api/tests.py
+++ app/api/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
+import unittest
+import pdb
+import json
+from rest_framework.test import APIRequestFactory, APIClient, RequestsClient
+from .models import CoffeeShop, FavoriteShop
 
-# Create your tests here.
+
+class CoffeeShopTest(unittest.TestCase):
+    def setUp(self):
+        CoffeeShop.objects.create(
+            name='Starbucks',
+            is_closed=True,
+            location={"Tampa": "FL"},
+            yelp_id='fakeid123'
+        )
+        CoffeeShop.objects.create(
+            name='Kahwa Coffee',
+            is_closed=False,
+            location={"Tampa": "FL"},
+            yelp_id='fakeid321'
+        )
+        ging = CoffeeShop.objects.create(
+            name='Gingerbeard Coffee',
+            is_closed=True,
+            location={"Tampa": "FL"},
+            yelp_id='fakeid987'
+        )
+        client = RequestsClient()
+        self.response = client.get('http://localhost:8000/api/v1/coffeeshops/')
+        self.response_two = client.get(
+            'http://localhost:8000/api/v1/coffeeshops/{}'.format(ging.id))
+
+    def tearDown(self):
+        CoffeeShop.objects.all().delete()
+
+    def test_coffee_shop_can_be_retrieved(self):
+        shops = json.loads(self.response.content)
+        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(shops[0]['name'], 'Starbucks')
+        self.assertEqual(shops[1]['name'], 'Kahwa Coffee')
+        self.assertEqual(shops[2]['name'], 'Gingerbeard Coffee')
+        self.assertEqual(CoffeeShop.objects.count(), 3)
+
+    def test_can_return_a_single_coffee_shop(self):
+        shop = json.loads(self.response_two.content)
+        self.assertEqual(self.response_two.status_code, 200)
+        self.assertEqual(shop['name'], 'Gingerbeard Coffee')
+
+
+if __name__ == '__main__':
+    unittest.main()
--- app/app/settings.py
+++ app/app/settings.py
@@ -23,13 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.get_env('DJANGO_KEY')
+SECRET_KEY = os.getenv('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Set up Django Settings for running test suite
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CoffeeShopFinderBE.settings')
 
 # Application definition
 
@@ -43,6 +45,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'api'
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -80,8 +90,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USERNAME'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
